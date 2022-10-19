c6df6b0cd218e45e8b11a2b2ddc4f3552401b153
vincedejulius <dejuliusvince@gmail.com>
2022-10-19 19:18:19 -0400

    finished writing test for Employee library, tested to ensure it worked properly

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,21 @@
-const Emlployee = require("../lib/Employee")
+const Employee = require("../lib/Employee")
 
-describe("Employee", ()=>{
-  decsribe("test Employee's properties", ()=>{})
-
-  describe("test Employee's methods", ()=>{})
+describe("Employee", () => {
+  describe("test Employee's properties", () => {
+    it("test all properties", () => {
+      const employee = new Employee("Vince", 1, "dejuliusvince@gmail.com")
+      expect(employee.name).toEqual("Vince")
+      expect(employee.id).toEqual(1)
+      expect(employee.email).toEqual("dejuliusvince@gmail.com")
+    })
+  })
+  describe("test Employee's methods", () => {
+    it("test all methods", () => {
+      const employee = new Employee("Steve", 2, "steve@gmail.com")
+      expect(employee.getName()).toEqual("Steve")
+      expect(employee.getId()).toEqual(2)
+      expect(employee.getEmail()).toEqual("steve@gmail.com")
+      expect(employee.getRole()).toEqual("Employee")
+    })
+  })
 })
